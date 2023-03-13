624acc76a98b90d8bc1c88b2d4903628747570dc
Randy <randy708ochoa@gmail.com>
2023-03-13 17:46:18 -0600

    Transformando texto

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,5 +1,15 @@
-function saludar(nombre) {
-    return nombre;
+const input = document.getElementById("texto")
+function transformText() {
+    let lowerCase = input.value.toLowerCase()
+    let upperCase = input.value.toUpperCase()
+    let charAt = input.value.charAt(3)
+    let slice = input.value.slice(3, 10)
+
+
+    document.getElementById("lower-case").innerText = `Lower Case: ${lowerCase}`
+    document.getElementById("upper-case").innerText = `Upper Case: ${upperCase}`
+    document.getElementById("char-at").innerText = `Char At: ${charAt}`
+    document.getElementById("slice").innerText = `Slice: ${slice}`
 }
 
-saludar("Randy")
+input.addEventListener("keyup", transformText)
