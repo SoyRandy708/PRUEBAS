6e66fcafe5a2096ea9ce6c3d803ce9e3e9e0625e
Randy <randy708ochoa@gmail.com>
2023-03-13 16:08:59 -0600

    Prueba return function

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,17 +1,5 @@
-const modal = document.getElementById("modal");
-const abrirModal = document.getElementById("open-modal");
-const cerrarModal = document.getElementById("cerrar-modal");
-
-abrirModal.onclick = function() {
-    modal.style.display = "block";
-} 
-
-cerrarModal.onclick = function() {
-    modal.style.display = "none";
+function saludar(nombre) {
+    return nombre;
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+saludar("Randy")
